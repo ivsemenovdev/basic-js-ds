4447fee47f5e71e3870e2496843f26e70519e020
Ivan Semenov <ivsemenovdev@gmail.com>
2022-10-18 01:44:24 +0300

    feat: solve queue task

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,24 +13,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
+class Queue {
+
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    getUnderlyingList() {
+        let current = this.head;
+
+        while(current) {
+            console.log(current.value)
+            current = current.next;
+        }
+    }
+
+    enqueue(item) {
+        const node = new Node(item);
+
+        if (this.head) {
+            this.tail.next = node;
+            this.tail = node;
+        } else {
+            this.head = node;
+            this.tail = node;
+        }
+
+        this.length++;
+
+    }
+
+    dequeue() {
+        const current = this.head;
+        this.head = this.head.next;
+        this.length--;
+
+        return current.value;
+    }
+}
+
+
 module.exports = {
-  Queue
+    Queue
 };
