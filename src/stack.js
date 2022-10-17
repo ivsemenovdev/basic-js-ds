e0e5ef3a2546bb022863102f02115b96d6b87ebf
Ivan Semenov <ivsemenovdev@gmail.com>
2022-10-18 00:21:55 +0300

    feat: solve stack task

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor() {
+    this.items = [];
+  }
+
+  push(item) {
+    this.items.push(item);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.items.length === 0) {
+      return "Stack is empty" ;
+    } else {
+      return this.items.pop();
+    }
+
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.items[this.items.length - 1];
   }
 }
 
